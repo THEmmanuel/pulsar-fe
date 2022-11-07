f65b8d22bdb40fa573b1cf24106dd4447e9aae75
Emmanuel <emmayodayo@gmail.com>
2022-11-07 14:28:43 +0100

    add peer to peer ad component

---
+++ src/components/PeerToPeerAdHome/PeerToPeerAdHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './PeerToPeerAdHome.module.css';
+
+const PeerToPeerAdHome = () => {
+	return(
+		<div>
+			Ad stuff here
+		</div>
+	)
+}
+
+export default PeerToPeerAdHome;
--- src/containers/PeerToPeerHome/PeerToPeerHome.js
+++ src/containers/PeerToPeerHome/PeerToPeerHome.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './PeerToPeerHome.module.css';
+import PeerToPeerAdHome from '../../components/PeerToPeerAdHome/PeerToPeerAdHome';
 
 const PeerToPeerHome = () => {
 	return (
 		<div>
 			Peer to Peer ads
+			<PeerToPeerAdHome/>
 		</div>
 	)
 }
