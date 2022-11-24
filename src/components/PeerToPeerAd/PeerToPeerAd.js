ce70a8caefc5158d4f44d1e558e57ebc7bbebcca
Emmanuel <emmayodayo@gmail.com>
2022-11-24 19:00:52 +0100

    add peer to peer ad component

---
+++ src/components/PeerToPeerAd/PeerToPeerAd.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './PeerToPeerAd.module.css';
+
+const PeerToPeerAd = () => {
+	return (
+		<div>
+			Peer to peer
+		</div>
+	)
+}
+
+export default PeerToPeerAd;
