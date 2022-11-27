e3a57456289a2f1cd73052743b47aba13cc332d8
Emmanuel <emmayodayo@gmail.com>
2022-11-27 07:18:46 +0100

    add peer to peer page

---
+++ src/pages/PeerToPeerPage/PeerToPeerPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './PeerToPeerPage.module.css';
+import PeerToPeerAd from '../../components/PeerToPeerAd/PeerToPeerAd';
+
+const PeerToPeerPage = () => {
+	return (
+		<div>
+			<PeerToPeerAd/>
+			<PeerToPeerAd/>
+			<PeerToPeerAd/>
+			<PeerToPeerAd/>
+			<PeerToPeerAd/>
+		</div>
+	)
+}
+
+export default PeerToPeerPage;
