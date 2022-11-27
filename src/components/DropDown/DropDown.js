48966f87a4514548fad8dd9f1392773cf14fe26c
Emmanuel <emmayodayo@gmail.com>
2022-11-27 08:08:20 +0100

    update page structure and add styling

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './DropDown.module.css';
+import arrowImage from '../../assets/arrowImage.svg'
 
 const DropDown = () => {
 	return (
-		<div>
-			<span>Buy</span>
-			<span>arroe</span>
+		<div className={style.DropDown}>
+			<span className={style.DropDownText}>Buy</span>
+			<img src={arrowImage} alt="" />
 		</div>
 	)
 }
--- src/pages/PeerToPeerPage/PeerToPeerPage.js
+++ src/pages/PeerToPeerPage/PeerToPeerPage.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import style from './PeerToPeerPage.module.css';
 import PeerToPeerAd from '../../components/PeerToPeerAd/PeerToPeerAd';
+import DropDown from '../../components/DropDown/DropDown';
 
 const PeerToPeerPage = () => {
 	return (
-		<div className={style.PeerToPeerWrapper}>
-			<PeerToPeerAd/>
-			<PeerToPeerAd/>
-			<PeerToPeerAd/>
-			<PeerToPeerAd/>
-			<PeerToPeerAd/>
+		<div className={style.PeerToPeerPage}>
+			Peer to peer page
+
+			<div className={style.PeerToPeerDropdownWrapper}>
+				<DropDown />
+				<DropDown />
+			</div>
+
+			<div className={style.PeerToPeerWrapper}>
+				<PeerToPeerAd />
+				<PeerToPeerAd />
+				<PeerToPeerAd />
+				<PeerToPeerAd />
+				<PeerToPeerAd />
+			</div>
 		</div>
 	)
 }
