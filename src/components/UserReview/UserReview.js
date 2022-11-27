f89c5c38ea7d7a989a553cc97203796ea77eb21c
Emmanuel <emmayodayo@gmail.com>
2022-11-27 15:21:46 +0100

    add user review component

---
+++ src/components/UserReview/UserReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './UserReview.module.css';
+
+const UserReview = () => {
+	return (
+		<div>
+			UserReview
+		</div>
+	)
+}
+
+export default UserReview;
