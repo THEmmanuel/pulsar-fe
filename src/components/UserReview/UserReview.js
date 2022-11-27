d3023ca71eb20cbe79638312a9b1d65e80eb6e46
Emmanuel <emmayodayo@gmail.com>
2022-11-27 23:30:43 +0100

    update structure

--- src/components/UserReview/UserReview.js
+++ src/components/UserReview/UserReview.js
@@ -3,8 +3,26 @@ import style from './UserReview.module.css';
 
 const UserReview = () => {
 	return (
-		<div>
-			UserReview
+		<div className={style.UserReviewWrapper}>
+			<span className={style.UserReviewName}>
+				User name
+			</span>
+
+			<span className={style.UserReviewDate}>
+				User Date
+			</span>
+
+			<span className={style.UserPaymentMethod}>
+				Payment Method
+			</span>
+
+			<span className={style.UserReviewText}>
+				Review
+			</span>
+
+			<span>
+				Review Type
+			</span>
 		</div>
 	)
 }
--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -2,50 +2,57 @@ import React from 'react';
 import style from './UserPage.module.css';
 import UserStat from '../../components/UserStat/UserStat';
 import PeerToPeerAd from '../../components/PeerToPeerAd/PeerToPeerAd';
+import UserReview from '../../components/UserReview/UserReview';
 
 const UserPage = () => {
 	return (
 		<div className={style.UserPage}>
-			<div className={style.UserDetailsContainer}>
-				<span className={style.UserImage}>
-					User Image
-				</span>
-
-				<div className={style.UserNameDetails}>
-					<span className={style.UserName}>User Name</span>
-					<span className={style.UserJoinDate}>Join date</span>
-				</div>
-			</div>
-
-			<div className={style.UserStatsContainer}>
-				<UserStat />
-				<UserStat />
-				<UserStat />
-				<UserStat />
-			</div>
-
-			<div className={style.UserAdsWrapper}>
-				<div>
-					<span>
-						Buy
+			<div className={style.UserPageWrapper}>
+				<div className={style.UserDetailsContainer}>
+					<span className={style.UserImage}>
+						User Image
 					</span>
 
-					<div>
-						<PeerToPeerAd />
+					<div className={style.UserNameDetails}>
+						<span className={style.UserName}>User Name</span>
+						<span className={style.UserJoinDate}>Join date</span>
 					</div>
 				</div>
 
-				<div>
-					<span>
-						Sell
-					</span>
-					<PeerToPeerAd />
+				<div className={style.UserStatsContainer}>
+					<UserStat />
+					<UserStat />
+					<UserStat />
+					<UserStat />
 				</div>
 
-				<div>
-					<span>
-						Reviews
-					</span>
+				<div className={style.UserAdsWrapper}>
+					<div>
+						<span>
+							Buy
+						</span>
+
+						<div>
+							<PeerToPeerAd />
+						</div>
+					</div>
+
+					<div>
+						<span>
+							Sell
+						</span>
+						<PeerToPeerAd />
+					</div>
+
+					<div>
+						<span>
+							Reviews
+						</span>
+
+						<div>
+							<UserReview />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
