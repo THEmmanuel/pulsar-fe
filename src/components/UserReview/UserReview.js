cb3964b698fcd59d9a793e664d29ac4659eb93a3
Emmanuel <emmayodayo@gmail.com>
2022-11-28 23:36:00 +0100

    add styling to user review component

--- src/components/UserReview/UserReview.js
+++ src/components/UserReview/UserReview.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import style from './UserReview.module.css';
+import thumbsUp from '../../assets/thumbs_up.svg';
 
 const UserReview = () => {
 	return (
 		<div className={style.UserReviewWrapper}>
-			<span className={style.UserReviewName}>
-				User name
-			</span>
+			<div className={style.UserReview}>
+				<span className={style.UserReviewName}>
+					User name
+				</span>
 
-			<span className={style.UserReviewDate}>
-				User Date
-			</span>
+				<span className={style.UserReviewDate}>
+					User Date
+				</span>
 
-			<span className={style.UserPaymentMethod}>
-				Payment Method
-			</span>
+				<span className={style.UserPaymentMethod}>
+					Payment Method
+				</span>
 
-			<span className={style.UserReviewText}>
-				Review
-			</span>
+				<span className={style.UserReviewText}>
+					Review
+				</span>
 
-			<span>
-				Review Type
-			</span>
+				<span>
+					Review Type
+				</span>
+			</div>
 		</div>
 	)
 }
