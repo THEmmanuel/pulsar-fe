e8fb81409fbe2199007c829b4740de265ea1f6ee
Emmanuel <emmayodayo@gmail.com>
2022-11-29 02:29:30 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import SideBar from './containers/SideBar/SideBar';
+import NavBar from './components/NavBar/NavBar';
 import Dashboard from './pages/Dashboard/Dashboard';
 import WalletsPage from './pages/WalletsPage/WalletsPage';
 import Wallet from './pages/Wallet/Wallet';
@@ -22,56 +23,59 @@ function App() {
 		<Router>
 			<div className="App">
 				<SideBar />
+				<section >
+					<NavBar />
+					<Routes>
+						<Route
+							exact
+							path='/'
+							element={<Dashboard />}
+						/>
 
-				<Routes>
-					<Route
-						exact
-						path='/'
-						element={<Dashboard />}
-					/>
+						<Route
+							exact
+							path='/home'
+							element={<Dashboard />}
+						/>
 
-					<Route
-						exact
-						path='/home'
-						element={<Dashboard />}
-					/>
+						<Route
+							exact
+							path='/wallets'
+							element={<WalletsPage />}
+						/>
 
-					<Route
-						exact
-						path='/wallets'
-						element={<WalletsPage />}
-					/>
+						<Route
+							exact
+							path='/wallet'
+							element={<Wallet />}
+						/>
 
-					<Route
-						exact
-						path='/wallet'
-						element={<Wallet />}
-					/>
+						<Route
+							exact
+							path='/my-ads'
+							element={<MyAds />}
+						/>
 
-					<Route
-						exact
-						path='/my-ads'
-						element={<MyAds />}
-					/>
+						<Route
+							exact
+							path='/'
+							element={<Dashboard />}
+						/>
 
-					<Route
-						exact
-						path='/'
-						element={<Dashboard />}
-					/>
+						<Route
+							exact
+							path='/p2p'
+							element={<PeerToPeerPage />}
+						/>
 
-					<Route
-						exact
-						path='/p2p'
-						element={<PeerToPeerPage />}
-					/>
+						<Route
+							exact
+							path='/user-page'
+							element={<UserPage />}
+						/>
+					</Routes>
 
-					<Route
-						exact
-						path='/user-page'
-						element={<UserPage />}
-					/>
-				</Routes>
+				</section>
 			</div>
 		</Router>
 	);
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import style from './NavBar.module.css';
+
+const NavBar = () => {
+	return (
+		<nav>
+			NavBar
+		</nav>
+	)
+}
+
+export default NavBar;
