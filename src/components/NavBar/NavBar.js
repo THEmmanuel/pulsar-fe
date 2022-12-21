3624b99eeeffd2b7351976c79f081e9bf1295261
Emmanuel <emmayodayo@gmail.com>
2022-12-21 16:15:45 +0100

    add auth

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import style from './NavBar.module.css';
+import { useUser, UserButton } from '@clerk/clerk-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+	const { user } = useUser();
+	console.log(user)
+
 	return (
 		<nav className={style.NavBar}>
 			<div className={style.NavContent}>
 				<span>Page Title</span>
 
 				<div className={style.NavIntroduction}>
-					<div>Hi gabe</div>
-					<button>Login</button>
+					{
+						user ?
+							<div>
+								<span>Hi {user.username}</span>
+								<UserButton />
+							</div>
+							:
+							<Link to='/login'>
+								<button>Login</button>
+							</Link>
+					}
 				</div>
 			</div>
 		</nav>
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import style from './Login.module.css';
-import { SignIn } from '@clerk/clerk-react'
+import { useUser, SignIn } from '@clerk/clerk-react'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
-	return <div>
-		<SignIn />
-	</div>
-}
+	const { user } = useUser();
+	const navigate = useNavigate();
+	const goHome = () => {
+		navigate('/home')
+	}
+
+	if (user) {
+		goHome()
+	}
+
+	return (
+		<div className={style.AuthContainer}>
+			<SignIn />
+		</div>
+	)
+};
 
 export default Login;
