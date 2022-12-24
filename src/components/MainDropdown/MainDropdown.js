7dd8855538c59d82f2ca0e543212492734f62d0c
Emmanuel <emmayodayo@gmail.com>
2022-12-24 02:25:38 +0100

    add props and styling to main dropdown

--- src/components/MainDropdown/MainDropdown.js
+++ src/components/MainDropdown/MainDropdown.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import style from './MainDropdown.module.css';
+import arrowImage from '../../assets/arrowImage.svg';
 
-const MainDropdown = () => {
+const MainDropdown = props => {
 	return (
 		<div className={style.MainDropdownContainer}>
-			<span className={style.MainDropdownTitle}>Text</span>
+			<span className={style.MainDropdownTitle}>
+				{props.DropdownHeading}
+			</span>
 
 			<div className={style.MainDropdown}>
-				<span>USDC</span>
-				<span>USD Coin</span>
-				<span>arrow</span>
+				<div className={style.DropDownDetails}>
+					<span className={style.DropdownPrimaryText}>
+						{props.PrimaryText}
+					</span>
+
+					<span className={style.DropdownSecondaryText}>
+						{props.SecondaryText}
+					</span>
+				</div>
+				<img src={arrowImage} alt="" />
 			</div>
 		</div>
 	);
