838cfd125d8c8e8cdf894f48dfca264fe7a4ffe4
Emmanuel <emmayodayo@gmail.com>
2022-12-24 01:39:33 +0100

    update home p2p table and add styling

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -2,10 +2,10 @@ import React from 'react';
 import style from './DropDown.module.css';
 import arrowImage from '../../assets/arrowImage.svg'
 
-const DropDown = () => {
+const DropDown = props => {
 	return (
 		<div className={style.DropDown}>
-			<span className={style.DropDownText}>Buy</span>
+			<span className={style.DropDownText}>{props.DropDownText}</span>
 			<img
 				src={arrowImage}
 				alt=""
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import style from './Dashboard.module.css';
 import MarketCharts from '../../components/MarketCharts/MarketCharts';
 import PeerToPeerHome from '../../containers/PeerToPeerHome/PeerToPeerHome';
 import PortfolioHome from '../../containers/PortfolioHome/PortfolioHome';
+import DropDown from '../../components/DropDown/DropDown';
 
 const Dashboard = () => {
 	return (
@@ -15,12 +16,29 @@ const Dashboard = () => {
 
 				<div className={style.AdsAndPortfolio}>
 					<div className={style.PeerToPeerContainer}>
-						<div className={style.AdTypeWrapper}>
-							<span>USDT</span>
-							<span>NGN</span>
-							<span>Buy</span>
+						<div className={style.PeerToPeerHeadingWrapper}>
+							<div className={style.PeerToPeerHeading}>
+								<div className={style.AdTypeWrapper}>
+									<span className={style.PeerToPeerHeadingText}>
+										Peer To Peer Ads
+									</span>
+									<DropDown
+										DropDownText='USDT'
+									/>
+									<DropDown
+										DropDownText='NGN'
+									/>
+									<DropDown
+										DropDownText='Buy'
+									/>
+								</div>
+
+								<span>
+									See all P2P ads
+								</span>
+							</div>
 						</div>
-						
+
 						<div className={style.PeerToPeerWrapper}>
 							<PeerToPeerHome />
 							<PeerToPeerHome />
