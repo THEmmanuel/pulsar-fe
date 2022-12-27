ca1d4ba082ca3f02c334642e6b08751341321a16
Emmanuel <emmayodayo@gmail.com>
2022-12-27 02:17:19 +0100

    add state and useeffect

--- src/pages/WalletsPage/WalletsPage.js
+++ src/pages/WalletsPage/WalletsPage.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './WalletsPage.module.css';
 import WalletCoin from '../../components/WalletCoin/WalletCoin';
 import PrimaryCTA from '../../components/PrimaryCTA/PrimaryCTA.js';
+import axios from 'axios';
 import { Link } from 'react-router-dom'
 
+const API_URL = 'http://localhost:9000/'
+
 const WalletsPage = () => {
+	const [userObject, setUserObject] = useState([])
+
+	const getWalletDetails = () => {
+		axios.get(`${API_URL}users`)
+			.then(res => setUserObject(res.data.users))
+			.catch(err => console.log(err))
+		console.log('////get wallet details')
+	}
+
+	console.log(userObject)
+
+	useEffect(() => {
+		getWalletDetails()
+	}, []);
+
 	return (
 		<div className={style.WalletsPage}>
 			<div className={style.WalletCoinContainer}>
