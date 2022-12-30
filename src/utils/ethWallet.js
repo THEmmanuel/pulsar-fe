ffff91cfe8051428003a097eed192b087466fb0f
Emmanuel <emmayodayo@gmail.com>
2022-12-30 01:33:45 +0100

    add send eth function

--- src/utils/ethWallet.js
+++ src/utils/ethWallet.js
@@ -1,9 +1,70 @@
-import { ethers } from 'ethers';
+import {
+	ethers
+} from 'ethers';
+
+const network = 'goerli';
+const provider = ethers.getDefaultProvider(network);
+let balance = 0
 
 export const getETHBalance = async (address) => {
-	const network = 'goerli';
-	const provider = ethers.getDefaultProvider(network);
-	const balance = await provider.getBalance(address);
+	balance = await provider.getBalance(address);
 	const EthBalance = ethers.utils.formatEther(balance);
 	return EthBalance;
 }
+
+export const SendETH = async (senderAddress, address, amount, key) => {
+	const wallet = new ethers.Wallet(key, provider);
+	balance = await provider.getBalance(senderAddress)
+
+	if (balance.lt(amount)) {
+		throw new Error(`Insufficient balance. Required: ${amount.toString()}, available: ${balance.toString()}`);
+	}
+
+	const tx = {
+		to: address,
+		value: ethers.utils.parseEther(amount.toString())
+	};
+
+	const gasEstimate = await provider.estimateGas(tx);
+	const gasCostInEther = ethers.utils.formatEther(gasEstimate);
+	console.log(`Estimated gas cost: ${gasCostInEther} ETH`);
+
+	const signedTx = await wallet.sign(tx);
+	const response = await provider.sendTransaction(signedTx);
+	return response;
+}
+
+
+// import { ethers } from 'ethers';
+
+// const network = 'goerli';
+// const provider = ethers.getDefaultProvider(network);
+
+// export const getETHBalance = async (address) => {
+// 	const balance = await provider.getBalance(address);
+// 	const EthBalance = ethers.utils.formatEther(balance);
+// 	return EthBalance;
+// }
+
+// export const SendETH = async (address, amount, key) => {
+// 	const wallet = new ethers.Wallet(key, provider);
+// 	const balance = await wallet.getBalance();
+
+// 	if (balance.lt(amount)) {
+// 		throw new Error(`Insufficient balance. Required: ${amount.toString()}, available: ${balance.toString()}`);
+// 	}
+
+// 	const tx = {
+// 		to: address,
+// 		value: ethers.utils.parseEther(amount.toString())
+// 	};
+
+// 	const gasEstimate = await provider.estimateGas(tx);
+// 	const gasCostInEther = ethers.utils.formatEther(gasEstimate);
+// 	console.log(`Estimated gas cost: ${gasCostInEther} ETH`);
+
+// 	const signedTx = await wallet.sign(tx);
+// 	const response = await provider.sendTransaction(signedTx);
+
+// 	return response;
+// }
