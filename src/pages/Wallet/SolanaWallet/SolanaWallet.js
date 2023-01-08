94b6a7278b19999a710a125d5e1b7c7d0fc28fcd
Emmanuel <emmayodayo@gmail.com>
2023-01-08 16:40:59 +0100

    add solana wallet component

---
+++ src/pages/Wallet/SolanaWallet/SolanaWallet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './SolanaWallet.module.css';
+
+const SolanaWallet = () => {
+	return (
+		<div>
+			Solana Wallet
+		</div>
+	)
+}
+
+export default SolanaWallet;
