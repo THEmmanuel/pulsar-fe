ae5a88d6c6716d82737bd8412d1fc383b6eae2e8
Emmanuel <emmayodayo@gmail.com>
2023-01-13 15:37:08 +0100

    add form input to create ad page

--- src/pages/CreateAdPage/CreateAdPage.js
+++ src/pages/CreateAdPage/CreateAdPage.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import style from './CreateAdPage.module.css';
 import PrimaryCTA from '../../components/PrimaryCTA/PrimaryCTA';
+import FormInput from '../../components/FormInput/FormInput';
 
 const CreateAdPage = () => {
 	return (
 		<div>
 			<span>Create Ad page</span>
+
+			<div>
+				<FormInput
+					title='Rate'
+				/>
+
+				<FormInput
+					title='Available USDT'
+				/>
+
+				<FormInput
+					title='Lowest Order Limit'
+				/>
+
+				<FormInput
+					title='Highest Order Limit'
+				/>
+
+
+			</div>
 			<PrimaryCTA />
 		</div>
 	)
