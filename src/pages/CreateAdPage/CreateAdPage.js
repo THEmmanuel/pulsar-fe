42ca95c40990ff394247312c70764da70f03b373
Emmanuel <emmayodayo@gmail.com>
2023-01-13 15:43:25 +0100

    add styling to create ad page

--- src/pages/CreateAdPage/CreateAdPage.js
+++ src/pages/CreateAdPage/CreateAdPage.js
@@ -7,27 +7,28 @@ const CreateAdPage = () => {
 	return (
 		<div>
 			<span>Create Ad page</span>
+			<div className={style.FormInputContainer}>
+				<div className={style.FormInputWrapper}>
+					<FormInput
+						title='Rate'
+					/>
 
-			<div>
-				<FormInput
-					title='Rate'
-				/>
+					<FormInput
+						title='Available USDT'
+					/>
 
-				<FormInput
-					title='Available USDT'
-				/>
+					<FormInput
+						title='Lowest Order Limit'
+					/>
 
-				<FormInput
-					title='Lowest Order Limit'
-				/>
-
-				<FormInput
-					title='Highest Order Limit'
-				/>
+					<FormInput
+						title='Highest Order Limit'
+					/>
 
 
+				</div>
+				<PrimaryCTA />
 			</div>
-			<PrimaryCTA />
 		</div>
 	)
 }
