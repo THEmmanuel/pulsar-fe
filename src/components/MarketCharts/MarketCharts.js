93c7a56404bb588445b8b0a975404f83210941aa
Emmanuel <emmayodayo@gmail.com>
2023-02-09 03:36:12 +0100

    add coin icons and display

--- src/components/MarketCharts/MarketCharts.js
+++ src/components/MarketCharts/MarketCharts.js
@@ -2,6 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import style from './MarketCharts.module.css';
 
+import bitcoin from '../../assets/bitcoinIcon.svg';
+import ethereum from '../../assets/ethereumIcon.svg';
+import tether from '../../assets/tetherIcon.svg';
+import solana from '../../assets/solanaIcon.svg';
+
+const coinIcons = {
+	bitcoin,
+	ethereum,
+	tether,
+	solana
+}
 
 const MarketCharts = () => {
 	const [coinData, setCoinData] = useState({});
@@ -23,7 +34,6 @@ const MarketCharts = () => {
 					<th>Symbol</th>
 					<th>Price</th>
 					<th>24hr %</th>
-					<th>7d %</th>
 					<th>Market Cap</th>
 					<th>Trading Volume (24hr)</th>
 				</thead>
@@ -31,12 +41,13 @@ const MarketCharts = () => {
 				<tbody>
 					{Object.entries(coinData).map(([coin, info]) => (
 						<tr key={coin}>
-							<td className={style.ChartIcon}>Icon</td>
+							<td className={style.ChartIcon}>
+								<img src={coinIcons[coin]} alt="" />
+							</td>
 							<td className={style.CoinName}>{coin}</td>
 							<td className={style.CoinAbbreviation}>BTC</td>
 							<td className={style.CoinPrice}>{info.usd}</td>
 							<td className={style.CoinDiff}>{info.usd_24h_change}</td>
-							<td className={style.CoinDiff}>1.06</td>
 							<td className={style.CoinMarketCap}>{info.usd_market_cap}</td>
 							<td className={style.CoinTradingVolume}>{info.usd_24h_vol}</td>
 						</tr>
