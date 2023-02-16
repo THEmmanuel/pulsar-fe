3ed3cf10d0241f643313a43977fce980060b450b
Emmanuel <emmayodayo@gmail.com>
2023-02-16 13:14:15 +0100

    add transaction card component

---
+++ src/components/TransactionCard/TransactionCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import style from './TransactionCard.module.css';
+
+const TransactionCard = () => {
+	return (
+		<div>
+			<div>
+				<div>
+					image
+				</div>
+
+				<span>Username</span>
+			</div>
+		</div>
+	)
+}
+
+export default TransactionCard;
--- src/pages/TransactionsHistory/TransactionsHistory.js
+++ src/pages/TransactionsHistory/TransactionsHistory.js
@@ -1,10 +1,14 @@
 import React from "react";
 import style from './TransactionsHistory.module.css';
+import TransactionCard from "../../components/TransactionCard/TransactionCard";
 
 const TransactionsHistory = () => {
 	return (
 		<div>
 			Transactions History
+			<div>
+				<TransactionCard/>
+			</div>
 		</div>
 	)
 }
