a2db520807736c273740dd6d84d76d609b8fa605
Emmanuel <emmayodayo@gmail.com>
2023-02-16 20:01:06 +0100

    added page structure

--- src/pages/TransactionPage/TransactionPage.js
+++ src/pages/TransactionPage/TransactionPage.js
@@ -4,7 +4,32 @@ import style from './TransactionPage.module.css';
 const TransactionPage = () => {
 	return (
 		<div>
-			Transaction page
+			<div>
+				<span>Order Status</span>
+				<span>Order Image</span>
+				<span>Sucessful</span>
+			</div>
+
+			<div>
+				<span>Order Code</span>
+				<span>Order Token here</span>
+			</div>
+
+			<div>
+				<span>Your Order</span>
+				<div>
+					<span>Order Type</span>
+					<span>Buy or sell</span>
+				</div>
+			</div>
+
+			<div>
+				<span>Your Order</span>
+				<div>
+					<span>Order Type</span>
+					<span>Buy or sell</span>
+				</div>
+			</div>
 		</div>
 	)
 }
