d65872352043a49eff165523d6dad95e71b6cfb4
Emmanuel <emmayodayo@gmail.com>
2023-02-17 00:47:33 +0100

    add styling

--- src/pages/TransactionPage/TransactionPage.js
+++ src/pages/TransactionPage/TransactionPage.js
@@ -3,31 +3,33 @@ import style from './TransactionPage.module.css';
 
 const TransactionPage = () => {
 	return (
-		<div>
-			<div>
-				<span>Order Status</span>
-				<span>Order Image</span>
-				<span>Sucessful</span>
+		<div className={style.TransactionPage}>
+			<div className={style.TransactionStatusContainer}>
+				<span className={style.TransactionStatusHeading}>Order Status</span>
+				<div className={style.TransactionStatusIcon}>Order Image</div>
+				<span className={style.TransactionStatus}>Sucessful</span>
 			</div>
 
-			<div>
+			<div className={style.OrderTokenWrapper}>
 				<span>Order Code</span>
 				<span>Order Token here</span>
 			</div>
 
-			<div>
-				<span>Your Order</span>
-				<div>
-					<span>Order Type</span>
-					<span>Buy or sell</span>
+			<div className={style.OrderDetailsWrapper}>
+				<div className={style.OrderDetails}>
+					<span>Your Order</span>
+					<div>
+						<span>Order Type</span>
+						<span>Buy or sell</span>
+					</div>
 				</div>
-			</div>
 
-			<div>
-				<span>Your Order</span>
-				<div>
-					<span>Order Type</span>
-					<span>Buy or sell</span>
+				<div className={style.OrderDetails}>
+					<span>Your Order</span>
+					<div>
+						<span>Order Type</span>
+						<span>Buy or sell</span>
+					</div>
 				</div>
 			</div>
 		</div>
