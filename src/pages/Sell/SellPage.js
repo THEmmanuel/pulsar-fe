22fa60743df4c6314e2faea9f1c7645ac2484057
Emmanuel <emmayodayo@gmail.com>
2023-02-24 09:14:32 +0100

    set up order info

--- src/pages/Sell/SellPage.js
+++ src/pages/Sell/SellPage.js
@@ -1,40 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import style from './SellPage.module.css';
 
 import UserInformation from '../../components/UserInformation/UserInformation';
 import UserTerms from '../../components/UserTerms/UserTerms';
 import UserInformationContent from '../../components/UserInformationContent/UserInformationContent';
 
-
-const SellerAmountWrapper = () => {
-	return (
-		<div className={style.UserInformationContentWrapper}>
-			<div className={style.BuyPageInput}>
-				<input type="text" />
-				<span>USDT</span>
-				<span>All</span>
-			</div>
-
-			<div className={style.BuyPriceInformation}>
-				<span>Crypto Amount</span>
-				<span>600 USDT</span>
-			</div>
-
-			<div>
-				<span>Crypto Amount</span>
-				<span>600 USDT</span>
-			</div>
-
-			<div>
-				<span>Crypto Amount</span>
-				<span>600 USDT</span>
-			</div>
-		</div>
-	)
-};
-
+const API_URL = 'http://localhost:9000';
 
 const SellPage = () => {
+	const [adInfo, setAdInfo] = useState('');
+	const { id, amount } = useParams();
+
+	const getAdInfo = () => {
+		axios.get(`${API_URL}/p2p/${id}`)
+			.then(res => setAdInfo(res.data))
+	}
+
+	useEffect(() => {
+		getAdInfo()
+	}, [])
+
+
 	return (
 		<div className={style.SellPageWrapper}>
 			<div className={style.SellPageContainer}>
