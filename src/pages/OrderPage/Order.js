8751842e4e83638722b6644ee5fdbbebf18b1605
Emmanuel <emmayodayo@gmail.com>
2023-02-24 07:38:47 +0100

    add conditional rendering

--- src/pages/OrderPage/Order.js
+++ src/pages/OrderPage/Order.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './Order.module.css';
 import BuyPage from '../Buy/BuyPage';
 import SellPage from '../Sell/SellPage';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-const Order = () => {
+const API_URL = 'http://localhost:9000';
+const Order = (props) => {
+	const [adInfo, setAdInfo] = useState(null)
+	const { id, amount } = useParams();
+
+	const getAdInfo = () => {
+		axios.get(`${API_URL}/p2p/${id}`)
+			.then(res => setAdInfo(res.data))
+	}
+
+	useEffect(() => {
+		getAdInfo()
+	}, [])
+
+	let CurrentOrderPage;
+
+	if (adInfo && adInfo.adType === 'buy') {
+		CurrentOrderPage = <BuyPage />
+	}
+	if (adInfo && adInfo.adType === 'sell') {
+		CurrentOrderPage = <SellPage />
+	}
+
 	return (
-		<div>
-			Order page
-		</div>
+		adInfo ?
+			<div>
+				Order page
+				<span>{id}</span>
+				<span>{adInfo.adType}</span>
+				{CurrentOrderPage}
+			</div>
+			:
+			<span>Loading</span>
 	)
 }
 
