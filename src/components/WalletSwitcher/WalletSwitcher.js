3ef8d71f9e32e83d191c778eb7c37bbb78339c7b
Emmanuel <emmayodayo@gmail.com>
2024-08-15 22:25:13 +0100

    update

---
+++ src/components/WalletSwitcher/WalletSwitcher.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './WalletSwitcher.module.css';
+
+const WalletSwitcher = () => {
+	<div>
+		<span>All Wallets</span>
+		<span>Tunnel Wallets</span>
+		<span>Connected Wallets</span>
+	</div>
+}
+
+export default WalletSwitcher
--- src/index.js
+++ src/index.js
@@ -4,50 +4,50 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from '@clerk/clerk-react';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+// import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-	getDefaultConfig,
-	RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import {
-	mainnet,
-	polygon,
-	optimism,
-	arbitrum,
-	base,
-} from 'wagmi/chains';
-import {
-	QueryClientProvider,
-	QueryClient,
-} from "@tanstack/react-query";
+// import '@rainbow-me/rainbowkit/styles.css';
+// import {
+// 	getDefaultConfig,
+// 	RainbowKitProvider,
+// } from '@rainbow-me/rainbowkit';
+// import { WagmiProvider } from 'wagmi';
+// import {
+// 	mainnet,
+// 	polygon,
+// 	optimism,
+// 	arbitrum,
+// 	base,
+// } from 'wagmi/chains';
+// import {
+// 	QueryClientProvider,
+// 	QueryClient,
+// } from "@tanstack/react-query";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const frontendApi = process.env.REACT_APP_CLERK_FRONTEND_API;
 
-const config = getDefaultConfig({
-	appName: 'My RainbowKit App',
-	projectId: 'YOUR_PROJECT_ID',
-	chains: [mainnet, polygon, optimism, arbitrum, base],
-});
+// const config = getDefaultConfig({
+// 	appName: 'My RainbowKit App',
+// 	projectId: 'YOUR_PROJECT_ID',
+// 	chains: [mainnet, polygon, optimism, arbitrum, base],
+// });
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 
 
 root.render(
 	<React.StrictMode>
-		<WagmiProvider config={config}>
-			<QueryClientProvider client={queryClient}>
-				<RainbowKitProvider>
-					<ClerkProvider frontendApi={frontendApi}>
-						<App />
-					</ClerkProvider>
-				</RainbowKitProvider>
-			</QueryClientProvider>
-		</WagmiProvider>
+		{/* <WagmiProvider config={config}> */}
+		{/* <QueryClientProvider client={queryClient}> */}
+		{/* <RainbowKitProvider> */}
+		<ClerkProvider frontendApi={frontendApi}>
+			<App />
+		</ClerkProvider>
+		{/* </RainbowKitProvider> */}
+		{/* </QueryClientProvider> */}
+		{/* </WagmiProvider> */}
 	</React.StrictMode>
 );
 
